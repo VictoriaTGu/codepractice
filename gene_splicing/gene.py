d3b21d00b6e1c035b3c50d1e377bde36799e5b5e
Victoria T. Gu <victoriatingtinggu@gmail.com>
2016-10-05 14:27:50 -0700

    More efficient searching for pairs

--- gene_splicing/gene.py
+++ gene_splicing/gene.py
@@ -2,7 +2,7 @@ import unittest
 from linked_lst import SeqList
 
 
-def concat_seq_lst(seq_lst):
+def concat_sequences(seq_lst):
     unpaired_sequences = SeqList(seq_lst)
     head_to_tail, tail_to_head = find_substring_pairs(unpaired_sequences)
     first_seq = get_head(tail_to_head)
@@ -14,23 +14,50 @@ def concat_seq_lst(seq_lst):
         result = concat_two_seqs(result, next_seq, result_index + index)
         result_index += index
         seq = next_seq
-        return result
+    return result
 
 
 def find_substring_pairs(unpaired_sequences):
     head_to_tail = {}
     tail_to_head = {}
-    while len(unpaired_sequences) > len(head_to_tail):
-        current_seq = unpaired_sequences.peek()
-        for other_seq in unpaired_sequences:
-            if current_seq != other_seq:
-                index_of_match = get_substring_match(str(current_seq), str(other_seq))
-                if index_of_match is not None and index_of_match <= len(current_seq) / 2:
-                    head_to_tail[current_seq] = (other_seq, index_of_match)
-                    tail_to_head[other_seq] = current_seq
-                    unpaired_sequences.remove(current_seq)
-                    break
+    following_seq = None
+    counter = 0
+    while len(unpaired_sequences):
+        # we only need to search for previous seq
+        counter += 1
+        if following_seq:
+            current_seq = following_seq['seq']
+            unpaired_sequences.remove(current_seq)
+            following_seq = search_for_following_seq(current_seq, unpaired_sequences)
+        else:
+            current_seq = unpaired_sequences.peek()
+            unpaired_sequences.remove(current_seq)
+            prev_seq = search_for_prev_seq(current_seq, unpaired_sequences)
+            following_seq = search_for_following_seq(current_seq, unpaired_sequences)
+
+        # store information
+        if prev_seq:
+            head_to_tail[prev_seq['seq']] = (current_seq, prev_seq['index'])
+            tail_to_head[current_seq] = prev_seq['seq']
+        if following_seq:
+            head_to_tail[current_seq] = (following_seq['seq'], following_seq['index'])
+            tail_to_head[following_seq['seq']] = current_seq
     return head_to_tail, tail_to_head
+
+
+def search_for_prev_seq(current_seq, unpaired_sequences):
+    for other_seq in unpaired_sequences:
+        index_of_match = get_substring_match(str(other_seq), str(current_seq))
+        if index_of_match is not None and index_of_match <= len(current_seq) / 2:
+            return {'seq': other_seq, 'index': index_of_match}
+
+
+def search_for_following_seq(current_seq, unpaired_sequences):
+    following_seq = None
+    for other_seq in unpaired_sequences:
+        index_of_match = get_substring_match(str(current_seq), str(other_seq))
+        if index_of_match is not None and index_of_match <= len(current_seq) / 2:
+            return {'seq': other_seq, 'index': index_of_match}
 
 
 def get_substring_match(head_seq, tail_seq):
--- gene_splicing/gene_test.py
+++ gene_splicing/gene_test.py
@@ -5,7 +5,7 @@ from gene import get_substring_match
 from gene import concat_two_seqs
 from gene import find_substring_pairs
 from gene import get_head
-from gene import concat_seq_lst
+from gene import concat_sequences
 from linked_lst import SeqList
 from linked_lst import Node
 
@@ -41,7 +41,7 @@ class TestConcatSeqs(unittest.TestCase):
         snd_string = 'GGTACGG'
         assert get_substring_match(fst_string, snd_string) is None
 
-    def test_concat_seq_lst(self):
+    def test_concat_sequences(self):
         fst_string = 'CCAGTAC'
         snd_string = 'AGTACGG'
         assert concat_two_seqs(fst_string, snd_string, 2) == 'CCAGTACGG'
@@ -54,18 +54,50 @@ class TestConcatSeqs(unittest.TestCase):
         for head, (tail, ind) in m1.iteritems():
             assert str(head) in fst_to_snd_match
             assert fst_to_snd_match[str(head)] == (str(tail), ind)
+        assert len(fst_to_snd_match) == len(m1)
         for head, tail in m2.iteritems():
             assert str(head) in snd_to_fst_match
             assert snd_to_fst_match[str(head)] == str(tail)
+        assert len(snd_to_fst_match) == len(m2)
+
+    def test_find_substring_pairs_out_of_order(self):
+        seq_lst = SeqList(['CATT', 'GTCA', 'AAGT'])
+        fst_to_snd_match = {'AAGT': ('GTCA', 2), 'GTCA': ('CATT', 2)}
+        snd_to_fst_match = {'GTCA': 'AAGT', 'CATT': 'GTCA'}
+        m1, m2 = find_substring_pairs(seq_lst)
+        for head, (tail, ind) in m1.iteritems():
+            assert str(head) in fst_to_snd_match
+            assert fst_to_snd_match[str(head)] == (str(tail), ind)
+        assert len(fst_to_snd_match) == len(m1)
+        for head, tail in m2.iteritems():
+            assert str(head) in snd_to_fst_match
+            assert snd_to_fst_match[str(head)] == str(tail)
+        assert len(snd_to_fst_match) == len(m2)
+
 
     def test_get_head(self):
         snd_to_fst_match = {'GTCA': 'AAGT', 'CATT': 'GTCA'}
         assert get_head(snd_to_fst_match) == 'AAGT'
 
-    def test_concat_seq_lst(self):
+    def test_concat_sequences(self):
         seq_lst = ['AAGT', 'GTCA', 'CATT']
-        result = concat_seq_lst(seq_lst)
-        assert concat_seq_lst(seq_lst) == result
+        result = concat_sequences(seq_lst)
+        assert concat_sequences(seq_lst) == 'AAGTCATT'
+
+    def test_concat_sequences_out_of_order(self):
+        seq_lst = ['CATT', 'GTCA', 'AAGT']
+        result = concat_sequences(seq_lst)
+        assert concat_sequences(seq_lst) == 'AAGTCATT'
+
+    def test_concat_larger_sequences(self):
+        seq_lst = ['CATTG', 'GTCAT', 'AAGT', 'CAAA']
+        result = concat_sequences(seq_lst)
+        assert concat_sequences(seq_lst) == 'CAAAGTCATTG'
+
+    def test_even_larger_sequences(self):
+        seq_lst = ['ATTAGACCTG', 'CCTGCCGGAA', 'AGACCTGCCG', 'GCCGGAATAC']
+        result = concat_sequences(seq_lst)
+        assert concat_sequences(seq_lst) == 'ATTAGACCTGCCGGAATAC'
 
 
 if __name__ == "__main__":

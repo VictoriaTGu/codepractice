83d42de22e9ceb0d14648f30c817b00e5f3e1e6e
Victoria T. Gu <victoriatingtinggu@gmail.com>
2016-10-06 21:58:47 -0700

    Calculate allocations and sources

--- income-smoothing/classes.py
+++ income-smoothing/classes.py
@@ -5,12 +5,12 @@ DATE_FORMAT = '%Y-%m-%d'
 
 class AccountingEvent(object):
     # factory for generating the different types of incomes and expenses
-    def factory(common_attr, event_type, addl_info=None):
-        if event_type == "interval":
+    def factory(common_attr, event_frequency, addl_info=None):
+        if event_frequency == "interval":
             return IntervalEvent(common_attr=common_attr, period=addl_info['period'])
-        elif event_type == "monthly":
+        elif event_frequency == "monthly":
             return MonthlyEvent(common_attr=common_attr, days=addl_info['days'])
-        elif event_type == "oneTime":
+        elif event_frequency == "oneTime":
             return OneTimeEvent(common_attr=common_attr)
         else:
             raise ValueError
@@ -19,34 +19,57 @@ class AccountingEvent(object):
     def get_amount_on_date(self, date):
         raise NotImplementedError
 
-class MonthlyEvent(AccountingEvent):
-    def __init__(self, common_attr, days):
+    def __str__(self):
+        return ' '.join([
+            self.name, str(self.amount),
+            datetime.strftime(self.start_date, DATE_FORMAT)
+        ])
+
+    def __init__(self, common_attr):
         self.name = common_attr['name']
         self.amount = common_attr['amount']
         self.start_date = datetime.strptime(common_attr['start_date'], DATE_FORMAT)
+        self.event_type = common_attr['event_type']
+
+    def __eq__(self, other_event):
+        if self.name != other_event.name:
+            return False
+        if self.amount != other_event.amount:
+            return False
+        if self.start_date != other_event.start_date:
+            return False
+        if self.event_type != other_event.event_type:
+            return False
+        return True
+
+    def to_dict(self):
+        return {'type': self.event_type, 'name': self.name}
+
+
+class MonthlyEvent(AccountingEvent):
+    def __init__(self, common_attr, days):
+        super(MonthlyEvent, self).__init__(common_attr)
         self.days = days
+        self.leftover_amount = self.amount
 
     def __str__(self):
         return ' '.join([
-            'Monthly', self.name, str(self.amount),
-            datetime.strftime(self.start_date, DATE_FORMAT), str(self.days)
+            'Monthly', super(MonthlyEvent, self).__str__(), str(self.days)
         ])
 
     def get_amount_on_date(self, current_date):
         if current_date.day in self.days:
             return self.amount
 
+
 class IntervalEvent(AccountingEvent):
     def __init__(self, common_attr, period):
-        self.name = common_attr['name']
-        self.amount = common_attr['amount']
-        self.start_date = datetime.strptime(common_attr['start_date'], DATE_FORMAT)
+        super(IntervalEvent, self).__init__(common_attr)
         self.period = period
 
     def __str__(self):
         return ' '.join([
-            'Interval', self.name, str(self.amount),
-            datetime.strftime(self.start_date, DATE_FORMAT), str(self.period)
+            'Interval', super(IntervalEvent, self).__str__(), str(self.period)
         ])
 
     def get_amount_on_date(self, current_date):
@@ -54,16 +77,14 @@ class IntervalEvent(AccountingEvent):
         if timedelta.days >= 0 and timedelta.days % self.period == 0:
             return self.amount
 
+
 class OneTimeEvent(AccountingEvent):
     def __init__(self, common_attr):
-        self.name = common_attr['name']
-        self.amount = common_attr['amount']
-        self.start_date = datetime.strptime(common_attr['start_date'], DATE_FORMAT)
+        super(OneTimeEvent, self).__init__(common_attr)
 
     def __str__(self):
         return ' '.join([
-            'OneTime', self.name, str(self.amount),
-            datetime.strftime(self.start_date, DATE_FORMAT)
+            'OneTime', super(OneTimeEvent, self).__str__()
         ])
 
     def get_amount_on_date(self, current_date):
--- income-smoothing/main.py
+++ income-smoothing/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from copy import deepcopy
 from datetime import datetime
 from datetime import timedelta
 import sys
@@ -11,11 +13,18 @@ START_DATE = '2016-01-01'
 
 def main():
     json_dict = json.load(sys.stdin)
-    income_lst = parse_events(json_dict['incomes'])
-    expense_lst = parse_events(json_dict['expenses'])
-    get_time_series(expense_lst)
+    incomes = parse_events(json_dict['incomes'], 'income')
+    expenses = parse_events(json_dict['expenses'], 'expense')
+    base = datetime.strptime(START_DATE, DATE_FORMAT)
+    dates_in_year = [base + timedelta(days=i) for i in xrange(365)]
+    allocations, sources = get_allocations_and_sources(incomes, expenses, dates_in_year)
+    events = generate_time_series(dates_in_year, allocations, sources, incomes, expenses)
+    sys.stdout.write(
+            json.dumps({"events": events}, sort_keys=True, indent=4, separators=(',', ': '))
+            + '\n'
+    )
 
-def parse_events(events):
+def parse_events(events, event_type):
     event_lst = []
     for event in events:
         event_lst.append(AccountingEvent.factory(
@@ -23,23 +32,87 @@ def parse_events(events):
                 'name': event.get('name'),
                 'amount': event.get('amount', 0),
                 'start_date': event.get('schedule', {}).get('start', START_DATE),
+                'event_type': event_type,
             },
-            event_type=event['schedule']['type'],
+            event_frequency=event['schedule']['type'],
             addl_info=event['schedule']
         ))
     return event_lst
 
 
-def get_time_series(event_lst):
-    stack = LifoQueue()
-    base = datetime.strptime(START_DATE, DATE_FORMAT)
-    dates_in_year = [base + timedelta(days=i) for i in xrange(365)]
-    for date in dates_in_year:
-        for event in event_lst:
-            if event.get_amount_on_date(date):
-                stack.put(event)
-                print date, str(event)
+def get_allocations_and_sources(incomes, expenses, dates):
+    available_sources = LifoQueue()
+    allocations, sources = defaultdict(list), defaultdict(list)
+    for date in dates:
+        for income in incomes:
+            amount = income.get_amount_on_date(date)
+            if amount is not None:
+                income_with_metadata = {
+                        'date': date, 'event': income, 'amount_leftover': amount
+                }
+                available_sources.put(income_with_metadata)
+        for expense in expenses:
+            amount_owed = expense.get_amount_on_date(date)
+            while amount_owed is not None and amount_owed > 0:
+                if available_sources.empty():
+                    raise_insolvent()
 
+                income_with_metadata = available_sources.get()
+                amount_extracted = min([income_with_metadata['amount_leftover'], amount_owed])
+                income_with_metadata['amount_leftover'] -= amount_extracted
+                if income_with_metadata['amount_leftover'] > 0.0:
+                    available_sources.put(income_with_metadata)
+
+                expense_with_metadata = {
+                        'date': date, 'event': expense, 'amount': amount_extracted
+                }
+                allocations[
+                    (income_with_metadata['date'], income_with_metadata['event'])
+                ].append(expense_with_metadata)
+                income_copy = deepcopy(income_with_metadata)
+                income_copy['amount_allocated'] = amount_extracted
+                sources[(date, expense)].append(income_copy)
+
+                amount_owed -= amount_extracted
+
+    return allocations, sources
+
+def generate_time_series(dates, allocations, sources, incomes, expenses):
+    events = []
+    for date in dates:
+        for income in incomes:
+            if income.get_amount_on_date(date):
+                income_event_dict = income.to_dict()
+                income_event_dict['date'] = datetime.strftime(date, DATE_FORMAT)
+                income_event_dict['allocations'] = [
+                    {
+                        'date': datetime.strftime(allocation['date'], DATE_FORMAT),
+                        'name': allocation['event'].name,
+                        'amount': allocation['amount']
+                    }
+                    for allocation in allocations[(date, income)]
+                ]
+                events.append(income_event_dict)
+
+        for expense in expenses:
+            if expense.get_amount_on_date(date):
+                expense_event_dict = expense.to_dict()
+                expense_event_dict['date'] = datetime.strftime(date, DATE_FORMAT)
+                expense_event_dict['sources'] = [
+                    {
+                        'date': datetime.strftime(source['date'], DATE_FORMAT),
+                        'name': source['event'].name,
+                        'amount': source['amount_allocated']
+                    }
+                    for source in sources[(date, expense)]
+                ]
+                events.append(expense_event_dict)
+    return events
+
+
+def raise_insolvent():
+    sys.stdout.write(json.dumps({"error": 'Insolvent'}) + '\n')
+    sys.exit()
 
 
 if __name__ == "__main__":
